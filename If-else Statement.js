26c445321bba555059d55ea0c7ff8f506bbe5947
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:48:09 +0430

    Add files via upload

---
+++ If-else Statement.js
@@ -0,0 +1,7 @@
+If the condition for the if block is false, then the else block will be executed.
+
+if (condition) {
+// block of code to be executed if the condition is true
+} else {
+// block of code to be executed if the condition is false
+}
