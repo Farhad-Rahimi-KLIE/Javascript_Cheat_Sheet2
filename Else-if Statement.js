918229d155ac6c6aa6e11cc1889f4887b9464036
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:44:57 +0430

    Add files via upload

---
+++ Else-if Statement.js
@@ -0,0 +1,8 @@
+A basic if-else ladder
+if (condition1) {
+// block of code to be executed if condition1 is true
+} else if (condition2) {
+// block of code to be executed if the condition1 is false and condition2 is 
+} else {
+// block of code to be executed if the condition1 is false and condition2 is 
+}
