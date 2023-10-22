bc8f46182913625d419d60972095f6a3e6d7f20b
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-22 05:29:41 +0430

    Update Array.js

--- Array.js
+++ Array.js
@@ -1,2 +1,2 @@
-The array is a collection of data items of the same type. In simple terms, it is a variable that
+The array is a Collection of data items of the same type. In simple terms, it is a variable that
 contains multiple values.
