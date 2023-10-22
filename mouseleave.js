e3b6558b1ced515d0f25e606e3b5402706097884
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-22 05:44:21 +0430

    Update mouseleave.js

--- mouseleave.js
+++ mouseleave.js
@@ -1,4 +1,4 @@
-Fired when an element is exited by the mouse arrow
+//Fired when an element is exited by the mouse arrow
 element.addEventListener('mouseleave', ()=>{
 // Code to be executed when the event is fired
 });
