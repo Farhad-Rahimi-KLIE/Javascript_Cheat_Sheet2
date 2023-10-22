d6762fa171607d997b7a625009bc567a01a31bbc
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-22 05:40:01 +0430

    Update confirm.js

--- confirm.js
+++ confirm.js
@@ -1,3 +1,3 @@
-The window.confirm() instructs the browser to display a dialog with an optional message, and to
-wait until the user either confirms or cancels
+//The window.confirm() instructs the browser to display a dialog with an optional message, and to
+//wait until the user either confirms or cancels
 window.confirm('Are you sure?')
