4d62eeef31b96bb607f7adea244bee9d1f92c2f6
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-22 05:32:58 +0430

    Update Iterative Statements (Loops).js

--- Iterative Statements (Loops).js
+++ Iterative Statements (Loops).js
@@ -1,2 +1,2 @@
-Iterative statement facilitates programmer to execute any block of code lines repeatedly and can
-be controlled as per conditions added by the programmer.
+//Iterative statement facilitates programmer to execute any block of code lines repeatedly and can
+//be controlled as per conditions added by the programmer.
