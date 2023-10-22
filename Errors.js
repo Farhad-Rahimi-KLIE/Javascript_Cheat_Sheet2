4a35c6457ec379aaf5aef97716188021935c9b63
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-22 05:31:24 +0430

    Update Errors.js

--- Errors.js
+++ Errors.js
@@ -1,3 +1,3 @@
-Errors are thrown by the compiler or interpreter whenever they find any fault in the code, and it
-can be of any type like syntax error, run-time error, logical error, etc. JS provides some functions
-to handle the errors.
+//Errors are thrown by the compiler or interpreter whenever they find any fault in the code, and it
+//can be of any type like syntax error, run-time error, logical error, etc. JS provides some functions
+//to handle the errors.
