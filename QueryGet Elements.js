8f4d9ef14b474bbbdbdbb493d87710cda7b7bf95
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-22 05:36:13 +0430

    Update QueryGet Elements.js

--- QueryGet Elements.js
+++ QueryGet Elements.js
@@ -1,2 +1,2 @@
-The browser creates a DOM (Document Object Model) whenever a web page is loaded, and with
-the help of HTML DOM, one can access and modify all the elements of the HTML document.
+//The browser creates a DOM (Document Object Model) whenever a web page is loaded, and with
+//the help of HTML DOM, one can access and modify all the elements of the HTML document.
