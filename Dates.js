23a2bac0caff50bf8b40c51f41316454155046d3
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-22 05:30:41 +0430

    Update Dates.js

--- Dates.js
+++ Dates.js
@@ -1,2 +1,2 @@
-Date object is used to get the year, month and day. It has methods to get and set day, month,
-year, hour, minute, and seconds.
+//Date object is used to get the year, month and day. It has methods to get and set day, month,
+//year, hour, minute, and seconds.
