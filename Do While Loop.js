ca75b86944bc11490a8bd21c2757291a41f4e4c7
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-22 05:30:54 +0430

    Update Do While Loop.js

--- Do While Loop.js
+++ Do While Loop.js
@@ -1,4 +1,4 @@
-A do while loop is executed at least once despite the condition being true or false
+//A do while loop is executed at least once despite the condition being true or false
 
 do {
 // run this code in block
