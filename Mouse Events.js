a8accc9c3bc393aeba89617ffe246c66e1a507d8
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-22 05:33:47 +0430

    Update Mouse Events.js

--- Mouse Events.js
+++ Mouse Events.js
@@ -1,2 +1,2 @@
-Any change in the state of an object is referred to as an Event. With the help of JS, you can
-handle events, i.e., how any specific HTML tag will work when the user does something.
+//Any change in the state of an object is referred to as an Event. With the help of JS, you can
+//handle events, i.e., how any specific HTML tag will work when the user does something.
