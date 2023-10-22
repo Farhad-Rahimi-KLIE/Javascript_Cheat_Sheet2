ed5fc4ce06a20d1be1143e8aea53febf40c25b66
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-22 05:43:05 +0430

    Update keypress.js

--- keypress.js
+++ keypress.js
@@ -1,4 +1,4 @@
-Fired when the user presses the key on the keyboard
+//Fired when the user presses the key on the keyboard
 element.addEventListener('keypress', ()=>{
 // Code to be executed when the event is fired
 });
