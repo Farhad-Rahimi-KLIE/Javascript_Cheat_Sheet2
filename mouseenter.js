95c2ada794022d9bd3bcddea25a5cbe4335583d9
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-22 05:44:04 +0430

    Update mouseenter.js

--- mouseenter.js
+++ mouseenter.js
@@ -1,4 +1,4 @@
-Fired when an element is entered by the mouse arrow
+//Fired when an element is entered by the mouse arrow
 element.addEventListener('mouseenter', ()=>{
 // Code to be executed when the event is fired
 });
