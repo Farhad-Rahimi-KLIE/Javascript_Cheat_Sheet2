47c299829a760691826923399ca1b64b4e51dd9c
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-22 05:32:29 +0430

    Update If Statement.js

--- If Statement.js
+++ If Statement.js
@@ -1,4 +1,4 @@
-The block of code to be executed, when the condition specified is true..
+//The block of code to be executed, when the condition specified is true..
 
 if (condition) {
 // block of code to be executed if the condition is true
