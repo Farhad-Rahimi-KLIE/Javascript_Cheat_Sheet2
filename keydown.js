31926a3e9ccbf1379d5922523ba0f02b56f858fe
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-22 05:42:51 +0430

    Update keydown.js

--- keydown.js
+++ keydown.js
@@ -1,4 +1,4 @@
-Fired when the user is pressing a key on the keyboard
+//Fired when the user is pressing a key on the keyboard
 element.addEventListener('keydown', ()=>{
 // Code to be executed when the event is fired
 });
